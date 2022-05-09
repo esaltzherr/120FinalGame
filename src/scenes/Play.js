296dbaff6db9de89f119e654f988260e7eb070fc
Mason Kubiak <mckubiak@icloud.com>
2022-05-09 15:15:55 -0700

    5/9/22 Monster Spawning/World Bounds

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -20,42 +20,54 @@ class Play extends Phaser.Scene {
         
     }
     create() {
-        
-        this.input.mouse.disableContextMenu();
+        this.physics.world.setBounds(0, 0, this.game.config.width, this.game.config.height);
+        this.physics.world.setBoundsCollision(true, true, true, true);
 
+        var r3 = this.add.rectangle(0, 0, this.game.config.width, this.game.config.height).setOrigin(0, 0);
+        r3.setStrokeStyle(3, 0x1a65ac);
+
+        this.input.mouse.disableContextMenu();
         this.add.text(0,0,"Controls: \nWASD\nSpace - Dash\nShift - Heal\nRightMouseButton - Sword\nLeftMouseButton - Shoot",  { font: '"Press Start 2P"' });
 
+        this.numMonsters = 10;
         this.monsters = this.physics.add.group();
         this.monsters.runChildUpdate = true;
+        this.spawnMonsters(this.numMonsters);
+        this.spawning = true;
+
         this.bullets = this.physics.add.group();
 
         var r1 = this.add.rectangle(200, 200, 148, 148, 0x6666ff);
 
         this.player = new Player(this, 0, 0, 'dudeDown');
-        
+        this.player.body.setCollideWorldBounds(true);
         this.cameras.main.startFollow(this.player);
 
-        var monster = []
-
-        monster.push(new BasicMonster(this, 100, 100, 'monster'));
-        monster.push(new BasicMonster(this, 500, 500, 'monster'));
-        this.monsters.addMultiple(monster);
-
-
+        //var monster = []
+        //monster.push(new BasicMonster(this, 100, 100, 'monster'));
+        //monster.push(new BasicMonster(this, 500, 500, 'monster'));
+        //this.monsters.addMultiple(monster);
 
         this.physics.add.collider(this.monsters, this.monsters);
         this.physics.add.collider(this.player, this.monsters, this.gotHit);
         this.physics.add.collider(this.monsters, this.bullets, this.destroy);
         this.physics.add.collider(this.player.knife, this.monsters, this.killMonster);
-
     }
 
     update() {
-       this.player.update();
+        this.player.update();
+
+        // at end of round, spawn 5 more monsters than last round
+        if(this.monsters.getLength() == 0 && !this.spawning) {
+            this.spawning = true;
+            this.numMonsters += 5;
+            this.spawnMonsters(this.numMonsters);
+        }
     }
     destroy(monster, bullet){
         monster.destroy();
         bullet.destroy();
+        console.log('dead');
     }
     killMonster(anything, monster){
         monster.destroy();
@@ -65,5 +77,32 @@ class Play extends Phaser.Scene {
         //player.health -= monster.damage;
         player.knockback(monster);
 
+    }
+
+    spawnMonsters(num) {
+        // spawn num times every second
+        let spawnTimer = this.time.addEvent({
+            delay: 1000,
+            repeat: num - 1,
+            callback: () => {
+                // randomly spawn monser between 200-300 pixels away
+                // LATER, WILL NEED TO CHANGE SO THEY DON'T SPAWN OUTSIDE WORLD
+                let randX = this.player.x + (Phaser.Math.Between(200, 300) * this.chooseSign());
+                let randY = this.player.y + (Phaser.Math.Between(200, 300) * this.chooseSign());                
+                this.monsters.add(new BasicMonster(this, randX, randY, 'monster'));
+                //console.log(randX + ', ' + randY)
+                
+                // check if done spawning
+                if(spawnTimer.getRepeatCount() == num - 1) {
+                    this.spawning = false;
+                }
+            },
+            callbackScope: this
+        });
+    }
+    
+    chooseSign() {
+        if(Math.floor(Math.random() * 2) % 2 == 0) { return 1; }
+        else { return -1; }
     }
 }
